b9600ff9d628765bd2afcd927299f76c32d41599
Kyril <Kyril@DESKTOP-N79IQOC>
2022-11-30 22:25:44 +0200

    Solving lab 1

---
+++ script.js
@@ -0,0 +1,110 @@
+/********************/
+// 1 - temrature task
+/********************/
+/*
+let temprature = parseInt(prompt("please enter today's temprature!"));
+console.log(temprature >= 30 ? "HOT" : "Cold");
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/***************/
+// 2 - user info
+/***************/
+/*
+let user = {
+  name: "",
+  bYear: 0,
+  age: 0,
+};
+
+function getInput() {
+  user.name = prompt("please enter your name!");
+
+  for (let i = 2; i > 0 && !isNaN(user.name); i--) {
+    user.name = prompt("please enter your name!");
+  }
+
+  if (!isNaN(user.name)) {
+    console.log("bad");
+    return;
+  }
+
+  user.bYear = parseInt(prompt("please enter your birth year!"));
+
+  for (let i = 2; i > 0 && isNaN(user.bYear) ; i--) {
+    user.bYear = prompt("please enter your birth year between 1940 and 2010!");
+  }
+
+  if (isNaN(user.bYear) ) {
+    console.log("bad");
+    return;
+  }
+
+  console.log(`user name : ${user.name}`);
+  console.log(`birth year : ${user.bYear}`);
+  console.log(`user age : ${2022 - user.bYear}`);
+}
+
+getInput();
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/***************************/
+// 4 - summation last digits
+/***************************/
+/*
+let n = parseInt(prompt("number 1 is ..."));
+let m = parseInt(prompt("number 2 is ..."));
+
+console.log(`summation of their last digits = ${(n % 10) + (m % 10)}`);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*****************/
+// 5 - Car Y speed
+/*****************/
+/*
+let time = parseInt(prompt("enter time .."));
+console.log(`car Y speed is ${time * 2} km in ${time} minutes`);
+*/
+
+
+
